0ae310cee8a5c89629d5d8f57574898acfaa4ded
unknown <maxime.soufer@gmail.com>
2022-02-11 16:18:14 -0500

    Ran 'prettier' and minor tweaks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
@@ -7,83 +7,98 @@ function ask(questionText) {
   });
 }
 
-let numberGuess = 50
-let maxNumber = 100
-let minNumber = 0
-
 async function start() {
+  let numberGuess = 50;
+  let maxNumber = 100;
+  let minNumber = 0;
+  let numberOfGuesses = 0;
 
-let numberGuess = 50
-let maxNumber = 100
-let minNumber = 0
-let numberOfGuesses = 0
+  console.log(
+    "\nLet's play a game where you (human) make up a number and I (computer) try to guess it."
+  );
 
-console.log("\nLet's play a game where you (human) make up a number and I (computer) try to guess it.")
-let answer = await ask(`Pick a number between 1 and 100(inclusive), and I will try to guess it...\n Type in "Go" when you're ready!\n`);
-let saniAnswer = answer.toLowerCase().replaceAll(" ","")
-while(saniAnswer !=="go"){ 
-  answer = await ask(`\nSorry I didn't quite catch that - Type in "Go" when you are!\n`);
-  saniAnswer = answer.toLowerCase().replaceAll(" ","")
-}
-
-console.log(`\nOk... Let me think...`);
-
-setTimeout(guess, 1500)
-
-async function guess(){
-  numberOfGuesses++
-  let confirm = await ask(`\nIs your number ${numberGuess}? (Y or N?) `)
-  let saniConfirm = confirm.toLowerCase().replaceAll(" ","")
-
-  while(saniConfirm !== "y" && saniConfirm !=="n") {
-  confirm = await ask(`\nSorry I didn't quite get that, answer with Y or N `);
-  saniConfirm = confirm.toLowerCase().replaceAll(" ","")
-}
-if (saniConfirm === "y"){
-  console.log(`\nYour number was ${numberGuess}! It took me ${numberOfGuesses} tries!`)
-
-  let retry = await ask("\nWould you like to play again? (Y or N)")
-  let saniRetry = retry.toLowerCase().replaceAll(" ","")
-  
-  while(saniRetry !== "y" && saniRetry !=="n") {
-    retry = await ask(`\nSorry I didn't quite get that, answer with Y or N `);
-    saniRetry = retry.toLowerCase().replaceAll(" ","")
+  let answer = await ask(
+    `Pick a number between 1 and 100(inclusive), and I will try to guess it...\n Type in "Go" when you're ready!\n`
+  );
+  let saniAnswer = answer.toLowerCase().replaceAll(" ", "");
+  while (saniAnswer !== "go") {
+    answer = await ask(
+      `\nSorry I didn't quite catch that - Type in "Go" when you are!\n`
+    );
+    saniAnswer = answer.toLowerCase().replaceAll(" ", "");
   }
-  if (saniRetry==="y"){
-    start()
-  } else if(saniRetry==="n"){
-    console.log("Ok! I'll see you next time!")
-    process.exit()
-  }
-} else { 
-  let higherOrLower = await ask("\nHmmm... Ok... Is it higher or lower? (H or L) ")
-  let saniHigherOrLower = higherOrLower.toLowerCase().replaceAll(" ","")
-  while(saniHigherOrLower !== "h" && saniHigherOrLower !=="l") {
-    higherOrLower = await ask(`\nSorry I didn't quite get that, answer with H or L `);
-    saniHigherOrLower = higherOrLower.toLowerCase().replaceAll(" ","")
-  }
-  if (saniHigherOrLower === "h"){
-    minNumber=numberGuess
-    numberGuess = Math.floor((maxNumber + numberGuess)/2)
-    if(numberGuess===minNumber){
-      console.log("\nIt could just be me but I feel like you're trying to cheat...\nLet's try again")
-      start()
+
+  console.log(`\nOk... Let me think...`);
+
+  setTimeout(guess, 1500);
+
+  async function guess() {
+    numberOfGuesses++;
+    let confirm = await ask(`\nIs your number ${numberGuess}? (Y or N?) `);
+    let saniConfirm = confirm.toLowerCase().replaceAll(" ", "");
+
+    while (saniConfirm !== "y" && saniConfirm !== "n") {
+      confirm = await ask(
+        `\nSorry I didn't quite get that, answer with Y or N `
+      );
+      saniConfirm = confirm.toLowerCase().replaceAll(" ", "");
     }
-    console.log(`\nOk... Let me think...`);
-    setTimeout(guess, 1500)
-  } else if(saniHigherOrLower === "l"){
-    maxNumber=numberGuess
-    numberGuess = Math.ceil((minNumber + numberGuess)/2)
-    if(numberGuess===maxNumber){
-      console.log("\nIt could just be me but I feel like you're trying to cheat...\nLet's try again")
-      start()
+    if (saniConfirm === "y") {
+      console.log(
+        `\nYour number was ${numberGuess}! It took me ${numberOfGuesses} tries!`
+      );
+
+      let retry = await ask("\nWould you like to play again? (Y or N)");
+      let saniRetry = retry.toLowerCase().replaceAll(" ", "");
+
+      while (saniRetry !== "y" && saniRetry !== "n") {
+        retry = await ask(
+          `\nSorry I didn't quite get that, answer with Y or N `
+        );
+        saniRetry = retry.toLowerCase().replaceAll(" ", "");
+      }
+      if (saniRetry === "y") {
+        start();
+      } else if (saniRetry === "n") {
+        console.log("Ok! I'll see you next time!");
+        process.exit();
+      }
+    } else {
+      let higherOrLower = await ask(
+        "\nHmmm... Ok... Is it higher or lower? (H or L) "
+      );
+      let saniHigherOrLower = higherOrLower.toLowerCase().replaceAll(" ", "");
+      while (saniHigherOrLower !== "h" && saniHigherOrLower !== "l") {
+        higherOrLower = await ask(
+          `\nSorry I didn't quite get that, answer with H or L `
+        );
+        saniHigherOrLower = higherOrLower.toLowerCase().replaceAll(" ", "");
+      }
+      if (saniHigherOrLower === "h") {
+        minNumber = numberGuess;
+        numberGuess = Math.floor((maxNumber + numberGuess) / 2);
+        if (numberGuess === minNumber) {
+          console.log(
+            "\nIt could just be me but I feel like you're trying to cheat...\nLet's try again"
+          );
+          start();
+        }
+        console.log(`\nOk... Let me think...`);
+        setTimeout(guess, 1500);
+      } else if (saniHigherOrLower === "l") {
+        maxNumber = numberGuess;
+        numberGuess = Math.ceil((minNumber + numberGuess) / 2);
+        if (numberGuess === maxNumber) {
+          console.log(
+            "\nIt could just be me but I feel like you're trying to cheat...\nLet's try again"
+          );
+          start();
+        }
+        console.log(`\nOk... Let me think...`);
+        setTimeout(guess, 1500);
+      }
     }
-    console.log(`\nOk... Let me think...`);
-    setTimeout(guess, 1500)  
   }
 }
-}
-}
 
-start()
-
+start();
